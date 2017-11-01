c83f458c6f71c07d6a57d8b7923e62f4c654a790
Lachtung <omer.roth@ymail.com>
2017-11-01 10:21:34 +0200

    setBold setItallic working

--- server.js
+++ server.js
@@ -13,14 +13,33 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 	console.log('a user connected');
 	var sessionColor = "black";
+	var sessionIsBold = false;
+	var sessionIsItallic = false;
+
 	socket.on('chat message', function(msg){
 		if (msg.match(/^\/setColor.*/i)) {
 			sessionColor = msg.split(" ")[1];
 			console.log("Session color changed to: " + sessionColor);
 		}
+		else if (msg.match(/^\/setBold.*/i)) {
+			if (sessionIsBold === false ) {
+				sessionIsBold = true;
+			}
+			else if (sessionIsBold === true){
+				sessionIsBold = false;
+			}
+		}
+		else if (msg.match(/^\/setItallic.*/i)) {
+			if (sessionIsItallic === false) {
+				sessionIsItallic = true;
+			}
+			else if (sessionIsItallic === true) {
+				sessionIsItallic = false;
+			}
+		}
 		else {
 			console.log(msg);
-			socket.broadcast.emit('chat message', msg,sessionColor);
+			socket.broadcast.emit('chat message', msg,sessionColor,sessionIsBold,sessionIsItallic);
 		}
 	});
 
